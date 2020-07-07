3ac012e8ed117c849ffc7c9c849de85395e71a43
Max <inflatableanchor@gmail.com>
2020-07-07 17:14:16 -0500

    added configuration properties

--- src/main/kotlin/com/max/contactbyweather/ContactByWeatherApplication.kt
+++ src/main/kotlin/com/max/contactbyweather/ContactByWeatherApplication.kt
@@ -1,9 +1,12 @@
 package com.max.contactbyweather
 
+import com.max.contactbyweather.config.ServiceProperties
 import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.context.properties.EnableConfigurationProperties
 import org.springframework.boot.runApplication
 
 @SpringBootApplication
+@EnableConfigurationProperties(ServiceProperties::class)
 class ContactByWeatherApplication
 
 fun main(args: Array<String>) {
