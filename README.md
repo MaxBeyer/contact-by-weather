695057faa8d191eb52c8e980096d6c5856aa377e
Max <inflatableanchor@gmail.com>
2020-07-07 13:23:28 -0500

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# contact-by-weather
+ contact individuals by different methods depending how the weather is that day
---
+++ src/main/kotlin/com/max/contactbyweather/ContactByWeatherApplication.kt
@@ -0,0 +1,11 @@
+package com.max.contactbyweather
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class ContactByWeatherApplication
+
+fun main(args: Array<String>) {
+	runApplication<ContactByWeatherApplication>(*args)
+}
