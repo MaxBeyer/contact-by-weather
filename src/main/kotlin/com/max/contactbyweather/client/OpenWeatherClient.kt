18490783a4dc9195a6a3b2131190c92f3481b03f
Max <inflatableanchor@gmail.com>
2020-07-09 07:47:53 -0500

    added units to config properties and set it to imperial

--- src/main/kotlin/com/max/contactbyweather/client/OpenWeatherClient.kt
+++ src/main/kotlin/com/max/contactbyweather/client/OpenWeatherClient.kt
@@ -7,9 +7,12 @@ import com.max.contactbyweather.config.ServiceProperties
 import com.max.contactbyweather.domain.OpenWeatherResponse
 import org.slf4j.LoggerFactory
 import org.springframework.http.HttpStatus
+import org.springframework.stereotype.Component
+import org.springframework.web.client.HttpStatusCodeException
 import org.springframework.web.client.RestTemplate
 import org.springframework.web.client.getForEntity
 
+@Component
 class OpenWeatherClient(
     private val properties: ServiceProperties,
     private val restTemplate: RestTemplate
@@ -18,17 +21,27 @@ class OpenWeatherClient(
     private val objectMapper = ObjectMapper().registerKotlinModule()
 
     fun getForcast(city: String): OpenWeatherResponse? {
-        val response = restTemplate.getForEntity<String>(
-                properties.openWeatherUrl,
-                hashMapOf(
-                        "city" to city,
-                        "appId" to properties.appId,
-                        "units" to properties.units
-                )
-        ).also { log.info("Response from OpenWeather API: $it") }
-        return when (response.statusCode) {
-            HttpStatus.OK -> objectMapper.readValue(response.body.toString())
-            else -> null
+        try {
+            val response = restTemplate.getForEntity<String>(
+                    properties.openWeatherUrl,
+                    hashMapOf(
+                            "city" to city,
+                            "appId" to properties.appId,
+                            "units" to properties.units
+                    )
+            ).also { log.debug("Response from OpenWeather API: $it") }
+            return when (response.statusCode) {
+                HttpStatus.OK -> objectMapper.readValue(response.body.toString())
+                else -> null
+            }
+        } catch (ex: HttpStatusCodeException) {
+            return if (ex.statusCode == HttpStatus.NOT_FOUND) {
+                log.error("${ex.statusCode}: '$city' could not be found as a city", ex.message)
+                null
+            } else {
+                log.error("An error occured while requesting OpenWeatherApi. status=${ex.statusCode} body=${ex.responseBodyAsString}", ex)
+                null
+            }
         }
     }
 }
